7e8d76202ce23b7b299a48e8a985645e33f21ab0
apehex <apehex@protonmail.com>
2024-08-21 21:08:55 +0200

    Adapt the pipeline to the new model

--- nestwork/tokun/pipeline.py
+++ nestwork/tokun/pipeline.py
@@ -21,15 +21,13 @@ CODE_US = b'\x1f'
 
 # ENCODE ######################################################################
 
-def encode(data: tf.Tensor, token_size: int, sample_size: int, dtype: tf.dtypes.DType=tf.dtypes.int32) -> tf.Tensor:
+def encode(data: tf.Tensor, token_size: int, sample_size: int, output_dtype: tf.dtypes.DType=tf.uint8) -> tf.Tensor:
     # factor 4 because of the UTF-32 encoding
     __dim = math.ceil(sample_size / token_size) * token_size
     # decode bytes from UTF-8
     __bytes = tf.strings.unicode_transcode(input=data, input_encoding='UTF-8', output_encoding='UTF-32-BE') # (B,)
     # decode byte strings to arrays of byte integers
-    __bytes = tf.io.decode_raw(__bytes, out_type=tf.uint8, fixed_length=__dim) # (B, 4 * S)
-    # cast to int32 as uint8 is not std
-    return tf.cast(__bytes, dtype=dtype)
+    return tf.io.decode_raw(__bytes, out_type=output_dtype, fixed_length=__dim, little_endian=False) # (B, 4 * S) or (B, S) depending on the dtype (1 or 4 bytes)
 
 # RESHAPE #####################################################################
 
@@ -56,28 +54,30 @@ def offset(data: tf.Tensor, ticks: int=1) -> tf.Tensor:
 
 # DECODE ######################################################################
 
-def decode(data: tf.Tensor) -> tf.Tensor:
+def codepoint(data: tf.Tensor) -> tf.Tensor:
     # make sure the dtype is large enough for UTF-32 codepoints
     __data = tf.cast(data, dtype=tf.dtypes.int32)
     # group the bytes 4 by 4
-    __shape = mlable.utils.divide_shape(shape=__data.shape, input_axis=-2, output_axis=-1, factor=4, insert=True)
+    __shape = mlable.utils.divide_shape(shape=data.shape, input_axis=-2, output_axis=-1, factor=4, insert=True)
     __bytes = tf.reshape(tensor=__data, shape=__shape)
     # compute the UTF-32-BE codepoints
-    __codes = mlable.ops.reduce_base(data=__bytes, base=256, axis=-1, keepdims=False)
-    # actually decode
-    __utf32 = tf.strings.unicode_encode(__codes, output_encoding='UTF-32-BE')
+    return mlable.ops.reduce_base(data=__bytes, base=256, axis=-1, keepdims=False)
+
+def decode(data: tf.Tensor) -> tf.Tensor:
+    # input = array of unicode codepoints
+    __utf32 = tf.strings.unicode_encode(data, output_encoding='UTF-32-BE')
     # convert to standard UTF-8
     return tf.strings.unicode_transcode(input=__utf32, input_encoding='UTF-32-BE', output_encoding='UTF-8')
 
 # > ###########################################################################
 
-def preprocess(text: str, token_size: int, expand: list=[]) -> tf.Tensor:
+def preprocess(text: str, token_size: int, output_dtype: tf.dtypes.DType=tf.uint8, expand: list=[]) -> tf.Tensor:
     # as tensor
     __data = tf.convert_to_tensor(text, dtype=tf.dtypes.string)
     # list of bytes
-    __bytes = encode(data=__data, token_size=token_size, sample_size=4 * len(text))
-    # partition or flatten
-    return reshape(data=__bytes, expand=expand)
+    __bytes = encode(data=__data, token_size=token_size, sample_size=4 * len(text), output_dtype=output_dtype)
+    # expand with unitary batch dim + cast
+    return tf.cast(reshape(data=__bytes, expand=expand), tf.float32)
 
 # < ###########################################################################
 
@@ -88,16 +88,24 @@ def unpack(data: tf.Tensor) -> list:
     __data = data.numpy().tolist()
     return [__s.decode('utf-8') for __s in __data]
 
-def postprocess(prediction: tf.Tensor, binary: bool=False, random: bool=False) -> str:
-    # from one-hot to UTF-32 bytes
-    __output = mlable.sampling.binary(prediction=prediction, threshold=0.5, random=random) if binary else mlable.sampling.categorical(prediction=prediction, random=random)
-    # flatten the groups of 4 bytes
+def postprocess(prediction: tf.Tensor, feature: str='binary', value: str='bytes', random: bool=False) -> str:
+    # interpret the feature axis
+    if feature == 'binary':
+        __output = mlable.sampling.binary(prediction=prediction, threshold=0.5, random=random)
+    elif feature == 'categorical':
+        __output = mlable.sampling.categorical(prediction=prediction, random=random)
+    else:
+        __output = tf.squeeze(tf.cast(tf.round(prediction), tf.int32), axis=-1)
+    # merge the bytes into codepoints
+    if value == 'bytes':
+        __output = codepoint(data=__output)
+    # decode the UTF-32-BE codepoints
     return decode(data=__output)
 
 # SAMPLING ####################################################################
 
 def sample(model: tf.keras.models.Model, text: str, **kwargs) -> tuple:
-    __x = preprocess(text=text, token_size=kwargs.get('token_size', 16), expand=kwargs.get('expand', [1]))
+    __x = preprocess(text=text, token_size=kwargs.get('token_size', 16), expand=kwargs.get('expand', [1]), output_dtype=kwargs.get('output_dtype', tf.uint8))
     __e = model._encoder(__x)
     __p = model._decoder(__e)
     __y = postprocess(__p, binary=kwargs.get('binary', False), random=kwargs.get('random', False))
