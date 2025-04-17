1dc1844984be9dfeee40121ecc3434775b98b0cd
apehex <apehex@protonmail.com>
2025-04-17 06:58:16 +0200

    Update to the latest mlable API

--- nestwork/tokun/pipeline.py
+++ nestwork/tokun/pipeline.py
@@ -6,9 +6,9 @@ import math
 
 import tensorflow as tf
 
-import mlable.ops
+import mlable.maths.ops
 import mlable.sampling
-import mlable.utils
+import mlable.shapes
 
 # UNICODE #####################################################################
 
@@ -58,10 +58,10 @@ def codepoint(data: tf.Tensor) -> tf.Tensor:
     # make sure the dtype is large enough for UTF-32 codepoints
     __data = tf.cast(data, dtype=tf.dtypes.int32)
     # group the bytes 4 by 4
-    __shape = mlable.utils.divide_shape(shape=data.shape, input_axis=-2, output_axis=-1, factor=4, insert=True)
+    __shape = mlable.shapes.divide(shape=data.shape, input_axis=-2, output_axis=-1, factor=4, insert=True)
     __bytes = tf.reshape(tensor=__data, shape=__shape)
     # compute the UTF-32-BE codepoints
-    return mlable.ops.reduce_base(data=__bytes, base=256, axis=-1, keepdims=False)
+    return mlable.maths.ops.reduce_base(data=__bytes, base=256, axis=-1, keepdims=False)
 
 def decode(data: tf.Tensor) -> tf.Tensor:
     # input = array of unicode codepoints
